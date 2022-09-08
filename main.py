0399c2d4bc995cb5091d422af2a83f8c0fc19bdb
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-08 19:26:55 +0200

    password logic

---
+++ main.py
@@ -0,0 +1,28 @@
+import string
+from random import shuffle, randint
+
+uppercase = list(string.ascii_uppercase)
+lowercase = (list(string.ascii_lowercase ))
+numbers = (list(string.digits))
+punc = (list(string.punctuation))
+
+class Password:
+    def __init__(self, len, show):
+        password = ""
+        for i in range(len):
+            password += uppercase[randint(0,25)]
+            password += lowercase[randint(0,25)]
+            password += numbers[randint(0,9)]
+            password += punc[randint(0,31)]
+        password = list(password)
+        shuffle(password)
+        password = "".join(password[0:len])
+        if show == 'y': print(password)
+        
+        
+
+
+len = input("How long?: ")
+show = input("Show Password in Terminal(y/n)?: ")
+
+x = Password(int(len), show.lower())
